04340570f2d9facb51041f7172293aab85ea8cb0
onasis08 <onasis08@naver.com>
2023-04-22 20:41:58 +0900

    4/22

--- data_structures/3_16.c
+++ data_structures/3_16.c
@@ -4,9 +4,28 @@
 typedef struct test {	// 구조체 정의
 	int data;
 	struct test *link;
-} test_str;	// 변수 선언
+} test_str;
 
 void main() {
 	int i;
 	test_str* ptest;
+	test_str* ptest2;
+
+	ptest = (double*)malloc(sizeof(test_str));	// malloc 함수를 이용한 동적 메모리 할당
+	ptest2 = (test_str*)malloc(sizeof(test_str));
+
+	// 동적 메모리 공간 실습 코드를 넣으시오.
+	if (!ptest || !ptest2)
+		exit(1);
+
+	ptest->data = 10;
+	ptest->link = ptest2;
+
+	ptest2->data = 20;
+	ptest2->link = NULL;
+
+	printf("%d %d", ptest->data, ptest->link->data);
+
+	free(ptest);
 }
+// 구조체 ptest의 타입이 왜 test_str이 아니라 double?
---
+++ data_structures/3_17.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() {
+	int* pi;
+
+	pi = (int*)malloc(100 * sizeof(char));
+
+	if (pi == NULL)
+		exit(1);
+
+	for (int i = 0; i < 100; i++) {
+		pi[i] = i;
+		printf("%d ", pi[i]);
+	}
+	
+	free(pi);
+}
+// malloc을 이용해 메모리를 할당하면 void*를 리턴하므로 사용하고자 하는 타입으로 캐스팅을 해야 한다.
+// 타입이 동일해야지 적절한 공간에 적절한 데이터를 쓰게 된다.
+// 1바이트 char형 메모리에 int 타입의 데이터를 쓰게 되는 오류가 생긴다.
