7cfff06110223115a79eb7e2f346d00789024961
onasis08 <onasis08@naver.com>
2023-04-27 18:48:45 +0900

    4/27

--- data_structures/17.c
+++ data_structures/17.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define DataType int
+#define DataType Student
 
 // 학생 정보를 저장하는 구조체 Student 정의
 typedef struct Student {
@@ -41,23 +41,30 @@ void init_list(LinkedST* ST) {
 }
 
 // 학생 출력
-
-
+void print_list(LinkedST* ST) {
+	Node* ptr = ST->head->rlink;
+	for (int i = 1; i <= ST->length; i++) {
+		printf("%s", ptr->data.name);
+		ptr = ptr->rlink;
+	}
+}
 
 // 이중 연결리스트의 마지막에 삽입
-void insert_node(Node* before, Node* new) {
-	new->llink = before;
-	new->rlink = before->rlink;
-	before->rlink = new;
+void insert_node_last(LinkedST *ST, Node* new) {
+	ST->length++;
+	new->llink = ST->head->llink;
+	new->rlink = ST->head;
+	new->llink->rlink = new;
 	new->rlink->llink = new;
 }
 // 이중 연결리스트에서 노드 삭제
-Node* delete_node(Node* head, Node* deleted) {
-	if (deleted == head) {
-		printf("헤드 노드는 삭제가 안 됩니다.");
+Node* delete_node(LinkedST *ST, Node* deleted) {
+	if (deleted == ST->head) {
+		printf("더미 노드는 삭제가 안 됩니다.");
 		return NULL;
 	}
 	else {
+		ST->length--;
 		deleted->llink->rlink = deleted->rlink;
 		deleted->rlink->llink = deleted->llink;
 		return deleted;
@@ -65,7 +72,7 @@ Node* delete_node(Node* head, Node* deleted) {
 }
 
 void main() {
-	int n;
+	int n, delete_order;
 	LinkedST students;
 
 	Student student1 = { "은서" };
@@ -74,7 +81,10 @@ void main() {
 	Student student4 = { "민주" };
 	Student student5 = { "지은" };
 
+	printf("학생 수를 입력하시오: ");
 	scanf("%d", &n);
+	printf("삭제할 순서를 입력하시오: ");
+	scanf("%d", &delete_order);
 	
 	// 리스트 초기화
 	init_list(&students);
@@ -85,4 +95,13 @@ void main() {
 	insert_node_last(&students, create_node(student3));
 	insert_node_last(&students, create_node(student4));
 	insert_node_last(&students, create_node(student5));
+
+
+		printf("제거된 사람: %s\n", delete_node(student1));
+		printf("남은 사람: ");
+	
+
+	// 출력
+	printf("학생: ");
+	print_list(&students);
 }
